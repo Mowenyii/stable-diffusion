fbbbb931c18ef8b394fcf5d512cfad579959c1a7
半夏花开 <1462959772@qq.com>
2022-11-03 11:29:46 +0800

    img2img_new.py可以提attention map，并且用来做mask

---
+++ ldm/modules/attention.py
@@ -0,0 +1,392 @@
+from inspect import isfunction
+import math
+import torch
+import torch.nn.functional as F
+from torch import nn, einsum
+from einops import rearrange, repeat
+
+from ldm.modules.diffusionmodules.util import checkpoint
+from collections import defaultdict
+
+
+
+heat_maps = defaultdict(list)# 77, (16,55,55)
+all_heat_maps = []
+
+
+def clear_heat_maps():
+    global heat_maps, all_heat_maps
+    heat_maps = defaultdict(list)
+    all_heat_maps = []
+
+
+def next_heat_map():
+    global heat_maps, all_heat_maps
+    all_heat_maps.append(heat_maps)
+    heat_maps = defaultdict(list)
+
+
+def get_global_heat_map(last_n: int = None, idx: int = None, factors=None):
+    global heat_maps, all_heat_maps
+
+    if idx is not None:
+        heat_maps2 = [all_heat_maps[idx]]
+    else:
+        heat_maps2 = all_heat_maps[-last_n:] if last_n is not None else all_heat_maps
+
+    if factors is None:
+        factors = {1, 2, 4, 8, 16, 32}
+
+    all_merges = []
+
+    for heat_map_map in heat_maps2:
+        merge_list = []
+
+        for k, v in heat_map_map.items():
+            if k in factors:
+                merge_list.append(torch.stack(v, 0).mean(0))
+
+        all_merges.append(merge_list)
+    #[10, 4, 77, 16, 64, 64]
+    maps = torch.stack([torch.stack(x, 0) for x in all_merges], dim=0)
+    return maps.sum(0).cuda().sum(2).sum(0)
+    #sum(2)这里把16那维全sum了，包括un-condition
+
+def exists(val):
+    return val is not None
+
+
+def uniq(arr):
+    return{el: True for el in arr}.keys()
+
+
+def default(val, d):
+    if exists(val):
+        return val
+    return d() if isfunction(d) else d
+
+
+def max_neg_value(t):
+    return -torch.finfo(t.dtype).max
+
+
+def init_(tensor):
+    dim = tensor.shape[-1]
+    std = 1 / math.sqrt(dim)
+    tensor.uniform_(-std, std)
+    return tensor
+
+
+# feedforward
+class GEGLU(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super().__init__()
+        self.proj = nn.Linear(dim_in, dim_out * 2)
+
+    def forward(self, x):
+        x, gate = self.proj(x).chunk(2, dim=-1)
+        return x * F.gelu(gate)
+
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, dim_out=None, mult=4, glu=False, dropout=0.):
+        super().__init__()
+        inner_dim = int(dim * mult)
+        dim_out = default(dim_out, dim)
+        project_in = nn.Sequential(
+            nn.Linear(dim, inner_dim),
+            nn.GELU()
+        ) if not glu else GEGLU(dim, inner_dim)
+
+        self.net = nn.Sequential(
+            project_in,
+            nn.Dropout(dropout),
+            nn.Linear(inner_dim, dim_out)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+def zero_module(module):
+    """
+    Zero out the parameters of a module and return it.
+    """
+    for p in module.parameters():
+        p.detach().zero_()
+    return module
+
+
+def Normalize(in_channels):
+    return torch.nn.GroupNorm(num_groups=32, num_channels=in_channels, eps=1e-6, affine=True)
+
+
+class LinearAttention(nn.Module):
+    def __init__(self, dim, heads=4, dim_head=32):
+        super().__init__()
+        self.heads = heads
+        hidden_dim = dim_head * heads
+        self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
+        self.to_out = nn.Conv2d(hidden_dim, dim, 1)
+
+    def forward(self, x):
+        b, c, h, w = x.shape
+        qkv = self.to_qkv(x)
+        q, k, v = rearrange(qkv, 'b (qkv heads c) h w -> qkv b heads c (h w)', heads = self.heads, qkv=3)
+        k = k.softmax(dim=-1)  
+        context = torch.einsum('bhdn,bhen->bhde', k, v)
+        out = torch.einsum('bhde,bhdn->bhen', context, q)
+        out = rearrange(out, 'b heads c (h w) -> b (heads c) h w', heads=self.heads, h=h, w=w)
+        return self.to_out(out)
+
+
+class SpatialSelfAttention(nn.Module):
+    def __init__(self, in_channels):
+        super().__init__()
+        self.in_channels = in_channels
+
+        self.norm = Normalize(in_channels)
+        self.q = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.k = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.v = torch.nn.Conv2d(in_channels,
+                                 in_channels,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+        self.proj_out = torch.nn.Conv2d(in_channels,
+                                        in_channels,
+                                        kernel_size=1,
+                                        stride=1,
+                                        padding=0)
+
+    def forward(self, x):
+        h_ = x
+        h_ = self.norm(h_)
+        q = self.q(h_)
+        k = self.k(h_)
+        v = self.v(h_)
+
+        # compute attention
+        b,c,h,w = q.shape
+        q = rearrange(q, 'b c h w -> b (h w) c')
+        k = rearrange(k, 'b c h w -> b c (h w)')
+        w_ = torch.einsum('bij,bjk->bik', q, k)
+
+        w_ = w_ * (int(c)**(-0.5))
+        w_ = torch.nn.functional.softmax(w_, dim=2)
+
+        # attend to values
+        v = rearrange(v, 'b c h w -> b c (h w)')
+        w_ = rearrange(w_, 'b i j -> b j i')
+        h_ = torch.einsum('bij,bjk->bik', v, w_)
+        h_ = rearrange(h_, 'b c (h w) -> b c h w', h=h)
+        h_ = self.proj_out(h_)
+
+        return x+h_
+
+
+class CrossAttention(nn.Module):
+    def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0.):
+        super().__init__()
+        inner_dim = dim_head * heads
+        context_dim = default(context_dim, query_dim)
+
+        self.scale = dim_head ** -0.5
+        self.heads = heads
+        # for slice_size > 0 the attention score computation
+        # is split across the batch axis to save memory
+        # You can set slice_size with `set_attention_slice`
+        self._slice_size = None
+
+        self.to_q = nn.Linear(query_dim, inner_dim, bias=False)
+        self.to_k = nn.Linear(context_dim, inner_dim, bias=False)
+        self.to_v = nn.Linear(context_dim, inner_dim, bias=False)
+
+        self.to_out = nn.Sequential(
+            nn.Linear(inner_dim, query_dim),
+            nn.Dropout(dropout)
+        )
+
+    def reshape_heads_to_batch_dim(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tensor.reshape(batch_size, seq_len, head_size, dim // head_size)
+        tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size * head_size, seq_len, dim // head_size)
+        return tensor
+
+    def reshape_batch_dim_to_heads(self, tensor):
+        batch_size, seq_len, dim = tensor.shape
+        head_size = self.heads
+        tensor = tensor.reshape(batch_size // head_size, head_size, seq_len, dim)
+        tensor = tensor.permute(0, 2, 1, 3).reshape(batch_size // head_size, seq_len, dim * head_size)
+        return tensor
+
+    @torch.no_grad()
+    def _up_sample_attn(self, x, factor, method: str = 'bicubic'):
+        weight = torch.full((factor, factor), 1 / factor ** 2, device=x.device)
+        weight = weight.view(1, 1, factor, factor)
+
+        h = w = int(math.sqrt(x.size(1)))
+        maps = []
+        x = x.permute(2, 0, 1)
+
+        with torch.cuda.amp.autocast(dtype=torch.float32):
+            for map_ in x:
+                map_ = map_.unsqueeze(1).view(map_.size(0), 1, h, w)
+                if method == 'bicubic':
+                    map_ = F.interpolate(map_, size=(64, 64), mode="bicubic", align_corners=False)
+                    maps.append(map_.squeeze(1))
+                else:
+                    maps.append(F.conv_transpose2d(map_, weight, stride=factor).squeeze(1).cpu())
+
+        maps = torch.stack(maps, 0).cpu()
+        return maps
+
+    def _attention(self, query, key, value, sequence_length, dim, use_context: bool = True):
+        batch_size_attention = query.shape[0]
+        hidden_states = torch.zeros(
+            (batch_size_attention, sequence_length, dim // self.heads), device=query.device, dtype=query.dtype
+        )
+        slice_size = self._slice_size if self._slice_size is not None else hidden_states.shape[0]
+        for i in range(hidden_states.shape[0] // slice_size):
+            start_idx = i * slice_size
+            end_idx = (i + 1) * slice_size
+            attn_slice = ( #16,4096,77
+                    torch.einsum("b i d, b j d -> b i j", query[start_idx:end_idx], key[start_idx:end_idx]) * self.scale
+            )
+            factor = int(math.sqrt(4096 // attn_slice.shape[1]))
+            attn_slice = attn_slice.softmax(-1) #取 -1维
+
+            if use_context:
+                if factor >= 1:
+                    factor //= 1
+                    maps = self._up_sample_attn(attn_slice, factor) #第一个有cross-atten的，factor==1
+                    global heat_maps
+                    heat_maps[factor].append(maps)
+                # print(attn_slice.size(), query.size(), key.size(), value.size())
+
+            attn_slice = torch.einsum("b i j, b j d -> b i d", attn_slice, value[start_idx:end_idx])
+
+            hidden_states[start_idx:end_idx] = attn_slice
+
+        # reshape hidden_states
+        hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
+        return hidden_states
+
+
+    def forward(self, x, context=None, mask=None):
+        h = self.heads
+
+        batch_size, sequence_length, dim = x.shape
+        use_context = context is not None
+
+        q = self.to_q(x)
+        context = default(context, x)
+        k = self.to_k(context)
+        v = self.to_v(context)
+
+        q = self.reshape_heads_to_batch_dim(q)# 16,4096,40
+        k = self.reshape_heads_to_batch_dim(k)# 16,77,40
+        v = self.reshape_heads_to_batch_dim(v)
+
+        # TODO(PVP) - mask is currently never used. Remember to re-implement when used
+
+        # attention, what we cannot get enough of
+        hidden_states = self._attention(q, k, v, sequence_length, dim, use_context=use_context)
+
+        return self.to_out(hidden_states)
+        '''
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+
+        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+
+        if exists(mask):
+            mask = rearrange(mask, 'b ... -> b (...)')
+            max_neg_value = -torch.finfo(sim.dtype).max
+            mask = repeat(mask, 'b j -> (b h) () j', h=h)
+            sim.masked_fill_(~mask, max_neg_value)
+
+        # attention, what we cannot get enough of
+        attn = sim.softmax(dim=-1)
+
+        out = einsum('b i j, b j d -> b i d', attn, v)
+        out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+        return self.to_out(out)
+        '''
+
+
+
+class BasicTransformerBlock(nn.Module):
+    def __init__(self, dim, n_heads, d_head, dropout=0., context_dim=None, gated_ff=True, checkpoint=True):
+        super().__init__()
+        self.attn1 = CrossAttention(query_dim=dim, heads=n_heads, dim_head=d_head, dropout=dropout)  # is a self-attention
+        self.ff = FeedForward(dim, dropout=dropout, glu=gated_ff)
+        self.attn2 = CrossAttention(query_dim=dim, context_dim=context_dim,
+                                    heads=n_heads, dim_head=d_head, dropout=dropout)  # is self-attn if context is none
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+        self.norm3 = nn.LayerNorm(dim)
+        self.checkpoint = checkpoint
+
+    def forward(self, x, context=None):
+        return checkpoint(self._forward, (x, context), self.parameters(), self.checkpoint)
+
+    def _forward(self, x, context=None):
+        x = self.attn1(self.norm1(x)) + x
+        x = self.attn2(self.norm2(x), context=context) + x
+        x = self.ff(self.norm3(x)) + x
+        return x
+
+
+class SpatialTransformer(nn.Module):
+    """
+    Transformer block for image-like data.
+    First, project the input (aka embedding)
+    and reshape to b, t, d.
+    Then apply standard transformer action.
+    Finally, reshape to image
+    """
+    def __init__(self, in_channels, n_heads, d_head,
+                 depth=1, dropout=0., context_dim=None):
+        super().__init__()
+        self.in_channels = in_channels
+        inner_dim = n_heads * d_head
+        self.norm = Normalize(in_channels)
+
+        self.proj_in = nn.Conv2d(in_channels,
+                                 inner_dim,
+                                 kernel_size=1,
+                                 stride=1,
+                                 padding=0)
+
+        self.transformer_blocks = nn.ModuleList(
+            [BasicTransformerBlock(inner_dim, n_heads, d_head, dropout=dropout, context_dim=context_dim)
+                for d in range(depth)]
+        )
+
+        self.proj_out = zero_module(nn.Conv2d(inner_dim,
+                                              in_channels,
+                                              kernel_size=1,
+                                              stride=1,
+                                              padding=0))
+
+    def forward(self, x, context=None):
+        # note: if no context is given, cross-attention defaults to self-attention
+        b, c, h, w = x.shape
+        x_in = x
+        x = self.norm(x)
+        x = self.proj_in(x)
+        x = rearrange(x, 'b c h w -> b (h w) c')
+        for block in self.transformer_blocks:
+            x = block(x, context=context)
+        x = rearrange(x, 'b (h w) c -> b c h w', h=h, w=w)
+        x = self.proj_out(x)
+        return x + x_in
---
+++ scripts/img2img_new.py
@@ -0,0 +1,347 @@
+
+import streamlit as st
+from einops import repeat
+# from streamlit_drawable_canvas import st_canvas
+import argparse, os, sys, glob
+from omegaconf import OmegaConf
+from PIL import Image
+from tqdm import tqdm
+import numpy as np
+import torch
+from main import instantiate_from_config
+from ldm.models.diffusion.ddim import DDIMSampler
+import time
+import os
+from torchvision.utils import make_grid
+import math
+from ldm.modules.attention import get_global_heat_map, clear_heat_maps
+import torch.nn.functional as F
+from einops import rearrange, repeat
+from matplotlib import pyplot as plt
+
+MAX_SIZE = 640
+
+# load safety model
+from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker
+from transformers import AutoFeatureExtractor
+from imwatermark import WatermarkEncoder
+import cv2
+
+safety_model_id = "CompVis/stable-diffusion-safety-checker"
+safety_feature_extractor = AutoFeatureExtractor.from_pretrained(safety_model_id)
+safety_checker = StableDiffusionSafetyChecker.from_pretrained(safety_model_id)
+wm = "StableDiffusionV1-Inpainting"
+wm_encoder = WatermarkEncoder()
+wm_encoder.set_watermark('bytes', wm.encode('utf-8'))
+
+
+def get_concat_h_multi_resize(im_list, n_rows, resample=Image.BICUBIC):
+    min_height = min(im.height for im in im_list)
+
+    im_list_resize = [
+        im.resize((int(im.width * min_height / im.height), min_height), resample=resample) for im in im_list
+    ]
+
+    max_width = max(im.width for im in im_list_resize)
+
+    n_cols = math.ceil(len(im_list) / n_rows)
+    total_width = (max_width) * n_cols  # compute total width
+    total_height = (min_height) * n_rows
+
+    dst = Image.new("RGB", (total_width, total_height))  # calculate the total height
+
+    pos_x = 0
+    pos_y = 0
+    for i in range(len(im_list_resize)):
+        im = im_list_resize[i]
+        dst.paste(im, (pos_y, pos_x))
+
+        pos_y += im.width
+
+        if (i + 1) % n_cols == 0:
+            # new row
+            pos_x += im.height
+            pos_y = 0
+
+    # print(" total: ", total_width, total_height)
+    return dst
+
+
+def numpy_to_pil(images):
+    """
+    Convert a numpy image or a batch of images to a PIL image.
+    """
+    if images.ndim == 3:
+        images = images[None, ...]
+    images = (images * 255).round().astype("uint8")
+    pil_images = [Image.fromarray(image) for image in images]
+
+    return pil_images
+
+def put_watermark(img):
+    if wm_encoder is not None:
+        img = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+        img = wm_encoder.encode(img, 'dwtDct')
+        img = Image.fromarray(img[:, :, ::-1])
+    return img
+
+def check_safety(x_image):
+    safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
+    x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
+    assert x_checked_image.shape[0] == len(has_nsfw_concept)
+    return x_checked_image, has_nsfw_concept
+
+
+def expand_m(m, n: int = 1, o=512, mode='bicubic'):
+    m = m.unsqueeze(0).unsqueeze(0) / n
+    m = F.interpolate(m.float().detach(), size=(o, o), mode='bicubic', align_corners=False)
+    m = (m - m.min()) / (m.max() - m.min() + 1e-8)
+    m = m.cpu().detach()
+
+    return m
+
+@st.cache(allow_output_mutation=True)
+def initialize_model(config, ckpt):
+    config = OmegaConf.load(config)
+    model = instantiate_from_config(config.model)
+
+    model.load_state_dict(torch.load(ckpt)["state_dict"], strict=False)
+
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = model.to(device)
+    sampler = DDIMSampler(model)
+
+    return sampler
+
+
+def make_batch_sd(
+        image,
+        mask,
+        txt,
+        device,
+        num_samples=1):
+    image = np.array(image.convert("RGB"))
+    image = image[None].transpose(0,3,1,2)
+    image = torch.from_numpy(image).to(dtype=torch.float32)/127.5-1.0
+
+    mask = np.array(mask.convert("L"))
+    mask = mask.astype(np.float32)/255.0
+    mask = mask[None,None]
+    mask[mask < 0.5] = 0
+    mask[mask >= 0.5] = 1
+    mask = torch.from_numpy(mask)
+
+    masked_image = image * (mask < 0.5)
+
+    batch = {
+            "image": repeat(image.to(device=device), "1 ... -> n ...", n=num_samples),
+            "txt": num_samples * [txt],
+            "mask": repeat(mask.to(device=device), "1 ... -> n ...", n=num_samples),
+            "masked_image": repeat(masked_image.to(device=device), "1 ... -> n ...", n=num_samples),
+            }
+    return batch
+
+
+def inpaint(sampler, image, mask, prompt, seed, scale, ddim_steps, num_samples=1, w=512, h=512):
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model = sampler.model
+
+    prng = np.random.RandomState(seed)
+    # model.get_first_stage_encoding(model.encode_first_stage(cc))
+    image0 = np.array(image.convert("RGB"))
+    image0 = image0[None].transpose(0, 3, 1, 2)
+    image0 = torch.from_numpy(image0).to(dtype=torch.float32) / 127.5 - 1.0
+    x0=model.get_first_stage_encoding(model.encode_first_stage(repeat(image0.to(device=device), "1 ... -> n ...", n=num_samples)))
+
+
+
+    # start_code = x0
+    start_code=prng.randn(num_samples, 4, h//8, w//8)
+    start_code = torch.from_numpy(start_code).to(device=device, dtype=torch.float32)
+    # TODO start_code也许可以不是噪声. 维度是[1,4,64,64]
+    with torch.no_grad():
+        with torch.autocast("cuda"):
+            batch = make_batch_sd(image, mask, txt=prompt, device=device, num_samples=num_samples)
+
+            c = model.cond_stage_model.encode(batch["txt"])
+
+            c_cat = list()#[1,5,64,64],5==1+4
+            for ck in model.concat_keys:
+                cc = batch[ck].float()
+                if ck != model.masked_image_key:# mask下采样到[1,1,64,64]，与VAE出来大小一样
+                    bchw = [num_samples, 4, h//8, w//8]
+                    cc = torch.nn.functional.interpolate(cc, size=bchw[-2:])
+                else:#[1,4,64,64]
+                    cc = model.get_first_stage_encoding(model.encode_first_stage(cc))#mask_img放进VAE
+                c_cat.append(cc)
+            c_cat = torch.cat(c_cat, dim=1)
+
+            # cond
+            cond={"c_concat": [c_cat], "c_crossattn": [c]}
+
+            # uncond cond
+            uc_cross = model.get_unconditional_conditioning(num_samples, "")
+            uc_full = {"c_concat": [c_cat], "c_crossattn": [uc_cross]}
+
+            shape = [model.channels, h//8, w//8]
+            samples_cfg, intermediates = sampler.sample(
+                    ddim_steps,
+                    num_samples,
+                    shape,
+                    cond,
+                    verbose=False,
+                    eta=1.0,
+                    unconditional_guidance_scale=scale,
+                    unconditional_conditioning=uc_full,
+                    x_T=start_code,
+                    x0=x0,
+            )
+            x_samples_ddim = model.decode_first_stage(samples_cfg)
+
+            result = torch.clamp((x_samples_ddim+1.0)/2.0,
+                                 min=0.0, max=1.0)
+
+            result = result.cpu().numpy().transpose(0,2,3,1)
+            result, has_nsfw_concept = check_safety(result)
+            result = result*255
+
+    result1 = [Image.fromarray(img.astype(np.uint8)) for img in result]
+    result1 = [put_watermark(img) for img in result1]
+    return result,result1
+
+
+if __name__ == "__main__":
+    # st.title("Stable Diffusion Inpainting")
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--indir",
+        type=str,
+        nargs="?",
+        help="dir containing image-mask pairs (`example.png` and `example_mask.png`)",
+    )
+    parser.add_argument(
+        "--outdir",
+        type=str,
+        nargs="?",
+        help="dir to write results to",
+    )
+    parser.add_argument(
+        "--steps",
+        type=int,
+        default=50,
+        help="number of ddim sampling steps",
+    )
+    parser.add_argument(
+        "--prompt",
+        type=str,
+        nargs="?",
+        default="A cat run in a field",#"A lion runs in the grassland",#
+        help="the prompt to render"
+    )
+    opt = parser.parse_args()
+    load_path="../configs/stable-diffusion/v1-inpainting-inference.yaml"
+    # ckpt_path="/home/wenyi_mo/stable-diffusion-main/models/ldm/inpainting_big/last.ckpt"
+    ckpt_path="/home/wenyi_mo/stable-diffusion-main/models/ldm/inpainting_big/sd-v1-5-inpainting.ckpt"
+    sampler = initialize_model(load_path, ckpt_path)
+
+    masks = sorted(glob.glob(os.path.join(opt.indir, "*_mask.png")))
+    image = [x.replace("_mask.png", ".png") for x in masks]
+    if image:
+        image = Image.open(image[0]).convert('RGB')
+        image_source=image
+        w, h = image.size
+        # print(f"loaded input image of size ({w}, {h})")
+        if max(w, h) > MAX_SIZE:
+            factor = MAX_SIZE / max(w, h)
+            w = int(factor*w)
+            h = int(factor*h)
+        width, height = map(lambda x: x - x % 64, (w, h))  # resize to integer multiple of 64
+        image = image.resize((width, height))
+        # print(f"resized to ({width}, {height})")
+
+        prompt = opt.prompt
+
+        seed = 42
+        num_samples = 1
+        scale = 5 #不能设太大，否则会全黑
+        ddim_steps = 10 #30
+
+        fill_color = "rgba(255, 255, 255, 0.0)"
+        stroke_width = 64
+        stroke_color = "rgba(255, 255, 255, 1.0)"
+        bg_color = "rgba(0, 0, 0, 1.0)"
+        # drawing_mode = "freedraw"
+
+        masks = Image.open(masks[0]).convert('RGB')
+        masks=np.array(masks).astype(np.uint8)
+        mask = masks[:, :, -1] > 0
+        clear_heat_maps()
+
+        if mask.sum() > 0:
+            mask = Image.fromarray(mask)
+
+            result_tensor,result = inpaint(
+                sampler=sampler,
+                image=image,
+                mask=mask,
+                prompt=prompt,
+                seed=seed,
+                scale=scale,
+                ddim_steps=ddim_steps,
+                num_samples=num_samples,
+                h=height, w=width
+            )
+            # st.write("Inpainted")
+            out_dir="../outputs/inpaint/"
+
+            now = int(time.time())
+            timeArr = time.localtime(now)
+            other_StyleTime = time.strftime("%m-%d/%H:%M", timeArr)
+            out_dir=out_dir+other_StyleTime+str(prompt)+"_scale"+str(scale) +'/'
+            if not os.path.exists(out_dir):
+                os.makedirs(out_dir)
+
+            i=0
+            for image in result:
+                pic_dir=out_dir+str(i)+".png"
+                # image.save(pic_dir)
+                # mask_dir = out_dir + str(i) + "_mask.png"
+                # mask.save(mask_dir)
+                get_concat_h_multi_resize(im_list=[image_source, mask,image], n_rows=1).save(pic_dir)
+                i=i+1
+
+        heat_maps = get_global_heat_map()#(idx=0)
+        #get_global_heat_map(idx=0).shape #TODO 可获得不同时间步的热力图
+        for num in range(len(prompt.split(' '))):
+
+            mplot = expand_m(heat_maps[num], 1)
+            mask1 = torch.ones_like(mplot)
+            mask1[mplot < 0.5 * mplot.max()] = 0
+            spotlit_im = torch.tensor(result_tensor[0]).cpu().float().detach()
+            # TODO Image.fromarray(result_tensor[0].astype(np.uint8)).save("./b.png")
+            # Image.fromarray(spotlit_im.numpy().astype(np.uint8)).save("./b1.png")
+
+            # spotlit_im2 = torch.cat((spotlit_im, (1 - mplot.squeeze(0)).pow(1)).permute(1, 2, 0), dim=3)
+            spotlit_im2 = torch.cat((spotlit_im.permute(2, 0, 1), (mplot.squeeze(0)).pow(1)), dim=0)
+            # a=spotlit_im.permute(0, 3, 1, 2) * mask1.squeeze(0)
+            #
+            # x_sample = 255. * rearrange(a.squeeze(0).cpu().numpy(), 'c h w -> h w c')
+            # Image.fromarray(x_sample.astype(np.uint8)).save("./b0.png")
+
+            # Image.fromarray(spotlit_im2.permute(1, 2, 0).numpy().astype(np.uint8)).save("./b1.png")
+
+            fig, ax = plt.subplots()
+
+            # ax.imshow(mplot.squeeze().numpy(), cmap='jet')
+            ax.imshow(result_tensor[0].astype(np.uint8))
+            ax.imshow(spotlit_im2.permute(1, 2, 0).numpy())
+            str_path=out_dir+"soft_"+prompt.split(' ')[num - 1]+".png"
+            plt.savefig(str_path)
+
+            # im2=spotlit_im.permute(2, 0, 1) * mask1.squeeze(0)
+            # ax.imshow(im2.permute(1, 2, 0).numpy())
+            # str_path =out_dir +"hard_"+ prompt.split(' ')[num - 1] + ".png"
+            # plt.savefig(str_path)
+        # print("enjoy")
+
+
